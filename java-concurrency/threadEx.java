d2aa0a7d5867a89b6f43ea49f016f9ba172ba69b
BadboyIntheLoop <chungnt28.uav@outlook.com>
2022-09-15 08:26:52 +0700

    Add Concurrency

---
+++ java-concurrency/threadEx.java
@@ -0,0 +1,8 @@
+package com.jenkov.java.concurrency.thread;
+
+class ThreadExample{
+    public static void main(String[] args){
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
